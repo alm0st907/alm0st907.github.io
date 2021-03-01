6e58384642e1af07dca1d3b0a442ce4e40ea8673
Garrett Rudisill <garrett.rudisill@gmail.com>
2021-02-28 21:54:29 -0800

    init the portfolio repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Coming soon to a browser near you...
         </p>
         <a
           className="App-link"
